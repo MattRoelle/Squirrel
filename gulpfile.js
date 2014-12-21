39d590db795cd08056646ca91d6db457d5315bda
Matthew Roelle <matthewroelle@gmail.com>
2014-12-21 00:48:40 -0800

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@ var gulp       = require('gulp');
 var sass       = require('gulp-sass');
 var concat     = require('gulp-concat');
 var uglify     = require('gulp-uglify');
-var del        = require('del');
 var sourcemaps = require('gulp-sourcemaps');
 var minifyCSS  = require('gulp-minify-css');
+var watch      = require('gulp-watch');
+var connect    = require('gulp-connect');
 
 gulp.task('build-js', function() {
   return gulp.src('src/js/*.js')
@@ -27,4 +28,25 @@ gulp.task('build-css', function() {
 
 gulp.task('build', ['build-js', 'build-css']);
 
-gulp.task('default', ['build']);
+gulp.task('server', function() {
+  connect.server({
+    root: './',
+    livereload: {
+      enable: true,
+      port: 45000
+    }
+  });
+});
+
+gulp.task('reload', function() {
+  return gulp.src('src/**/*')
+             .pipe(connect.reload());
+});
+
+
+gulp.task('watch', function() {
+  return gulp.watch('src/**/*', ['build', 'reload']);
+});
+
+
+gulp.task('default', ['watch', 'server']);
